d3393ab590578101c810fbf2e9516353e453ff11
Peter Lin <ptrln@me.com>
2013-04-10 09:12:44 -0700

    created newspaper project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppNewspaper::Application.config.secret_token = 'b880b7cab0c53c005e244696c7f272ca4ff409e011ec0ca0a6a960e60a5f353bd39da576ddb36b11ccc2359a3c4f61ae65f93f9926c43478ec8e3b9371acb3f3'
