7fed53f6216ca3ef1d500e287852ef4183e68a52
Peter Lin <ptrln@me.com>
2013-04-10 16:45:43 -0700

    complete phase 5

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -4,7 +4,7 @@ class Newspaper < ActiveRecord::Base
   belongs_to :editor, class_name: "User"
 
   has_many :subscription_plans, inverse_of: :newspaper
-    accepts_nested_attributes_for :subscription_plans, reject_if: :all_blank 
+    accepts_nested_attributes_for :subscription_plans, :reject_if => :all_blank 
   has_many :subscriptions, :through => :subscription_plans
   has_many :subscribers, :through => :subscriptions, :source => :user
 
